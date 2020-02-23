883444269ab23d586c62d4ebeb263fcba67843ef
findstr <findstr@sina.com>
2020-02-23 20:04:39 +0800

    refine htmldom

--- htmldom.lua
+++ htmldom.lua
@@ -1,16 +1,16 @@
 local M = {}
+local tconcat = table.concat
+local tremove = table.remove
 local nexttoken
 local tag = [["'</>=]]
 local T = {
 [tag:byte(1)] = function(str, start) -- "
-	start = start + 1
-	local e = str:find('"', start)
-	return str:sub(start, e - 1), e + 1
+	local e = str:find('[^\\]"', start)
+	return str:sub(start+1, e), e + 2
 end,
 [tag:byte(2)] = function(str, start) -- '
-	start = start + 1
-	local e = str:find("'", start)
-	return str:sub(start, e - 1), e + 1
+	local e = str:find("[^\\]'", start)
+	return str:sub(start+1, e), e + 2
 end,
 [tag:byte(3)] = function(str, start) -- <
 	local n = str:byte(start + 1)
@@ -44,9 +44,20 @@ end,
 
 local function plain(str, start)
 	local e = str:find("[%s><=/]", start)
-	assert(e > start, str:sub(start, start + 100))
+	assert(e > start)
 	return str:sub(start, e - 1), e
 end
+
+local function nexttext(str, start)
+	local start = str:find('[^%s]', start)
+	if not start then
+		return nil
+	end
+	local e = str:find("<", start)
+	assert(e > start)
+	return str:sub(start, e - 1), e
+end
+
 
 function nexttoken(str, start)
 	local start = str:find('[^%s]', start)
@@ -58,15 +69,8 @@ function nexttoken(str, start)
 	if not func then --plain
 		func = plain
 	end
-	local s, start = func(str, start)
-	return s, start
+	return func(str, start)
 end
-
-function nextattr(str, start)
-	local start = str:find('[/>%a]', start)
-	return nexttoken(str, start)
-end
-
 
 local node = {
 	value = function(self, buffer)
@@ -81,7 +85,7 @@ local node = {
 	text = function(self)
 		local tbl = {}
 		self:value(tbl)
-		return table.concat(tbl)
+		return tconcat(tbl)
 	end,
 	match = function(self, cond)
 		local match = true
@@ -89,8 +93,7 @@ local node = {
 		if class then
 			match = false
 			for _, v in pairs(self.class) do
-				local ok = v:find(class, 1, true)
-				if ok then
+				if v == class then
 					match = true
 					break
 				end
@@ -115,7 +118,7 @@ local node = {
 			class = nil,
 		}
 		local pattern = ".#"
-		for k in string.gmatch(method, "([.#]-[^.#%s]+)") do
+		for k in method:gmatch("([.#]-[^.#%s]+)") do
 			local n = k:byte(1)
 			if n == pattern:byte(1) then --.
 				cond.class = k:sub(2)
@@ -127,27 +130,19 @@ local node = {
 		end
 		local type, pairs = type, pairs
 		local nodes = {self}
-		local n = #nodes
-		::MATCH::
-		local j = 0
-		for i = 1, n do
-			local node = nodes[i]
-			for _, v in pairs(node.child) do
+		while true do
+			local n = tremove(nodes, 1)
+			if not n then
+				break
+			end
+			for _, v in pairs(n.child) do
 				if type(v) == "table" then
-					if v:match(cond) then
-						out[#out + 1] = v
-					else
-						j = j + 1
-						childs[j] = v
-					end
+					nodes[#nodes + 1] = v
 				end
 			end
-		end
-		if #out == 0 and j > 0 then
-			n = j
-			nodes = childs
-			childs = {}
-			goto MATCH
+			if n:match(cond) then
+				out[#out + 1] = n
+			end
 		end
 		return out
 	end,
@@ -161,7 +156,7 @@ local node = {
 }
 
 local nodemt = {__index = node}
-local special = {
+local selfclose = {
 	["META"] = true,
 	["LINK"] = true,
 	["INPUT"] = true,
@@ -169,135 +164,6 @@ local special = {
 	["BR"] = true,
 	["HR"] = true,
 }
-
-local opentag = {}
-
-local function parse(str, start)
-	local debug = str:sub(start, start + 350)
-	local s, start = nexttoken(str, start)
-	if not s then
-		return nil
-	end
-	assert(s == "<", s)
-	local name, start = nexttoken(str, start)
-	local attr = {}
-	local class = {}
-	local child = {}
-	local obj = {
-		name = name,
-		attr = attr,
-		class = class,
-		child = child,
-	}
-	local openid = #opentag + 1
-	opentag[openid] = name
-	setmetatable(obj, nodemt)
-	--attribute
-	local tk
-	while true do
-		local k, v
-		local back = start
-		tk, start = nextattr(str, start)
-		if tk == ">" or tk == "/>" then
-			break
-		end
-		k = tk
-		back = start
-		tk, start = nexttoken(str, start)
-		if tk == "=" then
-			v, start = nexttoken(str, start)
-			attr[k] = v
-			if k == "class" then
-				class[#class + 1] = v
-			end
-		else
-			attr[k] = true
-			start = back
-		end
-	end
-	if special[string.upper(name)] then
-		assert(tk == ">" or tk == "/>")
-		opentag[openid] = nil
-		return obj, start
-	end
-	if tk == "/>" then --close
-		opentag[openid] = nil
-		return obj, start
-	end
-	assert(tk == ">")
-	while true do
-		local back = start
-		tk, start = nexttoken(str, start)
-		if not tk then
-			opentag[openid] = nil
-			return obj
-		end
-		if tk == "</" then --close
-			local tk1
-			tk, start = nexttoken(str, start)
-			if not tk then
-				opentag[openid] = nil
-				return obj
-			end
-			tk1, start = nexttoken(str, start)
-			if not tk1 then
-				opentag[openid] = nil
-				return obj
-			end
-			assert(tk1 == ">", tk1)
-			if tk == name then --good
-				break
-			end
-			local opened = false
-			for _, v in pairs(opentag) do
-				if v == tk then
-					opened = true
-					break
-				end
-			end
-			if opened then --has opened
-				opentag[openid] = nil
-				return obj, back
-			end
-			--skip it
-		else
-			if tk:byte(1) == tag:byte(3) then -- '<'
-				local c
-				c, start = parse(str, back)
-				child[#child + 1] = c
-				if not start then
-					opentag[openid] = nil
-					return obj
-				end
-				assert(c, str:sub(start, start + 100))
-			else
-				local e = str:find("<", back)
-				local val = str:sub(back, e - 1)
-				start = e
-				child[#child + 1] = val
-			end
-		end
-	end
-	opentag[openid] = nil
-	return obj, start
-end
-
-local function trip(txt)
-	local buffer = {}
-	local i = 1
-	while i < #txt do
-		local _
-		local s = string.find(txt, "<script", i)
-		if not s then
-			buffer[#buffer + 1] = txt:sub(i, -1)
-			break
-		end
-		buffer[#buffer + 1] = txt:sub(i, s - 1)
-		_, i = string.find(txt, "</script[^>]*>", s + 1)
-		i = i + 1
-	end
-	return table.concat(buffer)
-end
 
 local html_unescape = {
 	['quot'] = '"',
@@ -309,18 +175,106 @@ local html_unescape = {
 
 function htmlunescape(html)
 	html = string.gsub(html, "&#(%d+);", function(s)
-		return string.char(tonumber(s))
+		return utf8.char(tonumber(s))
 	end)
 	html = string.gsub(html, "&(%a+);", html_unescape)
 	return html
 end
 
+local function parsenode(str, start)
+	--open tag
+	local tk, name, back
+	tk, start = nexttoken(str, start)
+	if not tk then
+		return
+	end
+	assert(tk == "<")
+	name, start = nexttoken(str, start)
+	local attr = {}
+	local class = {}
+	local child = {}
+	local obj = setmetatable({
+		name = name,
+		attr = attr,
+		class = class,
+		child = child,
+	}, nodemt)
+	--attribute
+	while true do
+		local x, val
+		tk, start = nexttoken(str, start)
+		if tk == ">" or tk == "/>" then
+			break
+		end
+		back = start
+		x, start = nexttoken(str, start)
+		if x == "=" then
+			local v
+			v, start = nexttoken(str, start)
+			if tk == "class" then
+				for c in v:gmatch("[^%s]+") do
+					class[#class + 1] = c
+				end
+				local a = attr[tk]
+				if a then
+					v = a .. " " .. v
+				end
+				attr[tk] = v
 
+			else
+				attr[tk] = v
+			end
+		else
+			attr[tk] = true
+			start = back
+		end
+	end
+	if tk == "/>" or selfclose[name:upper()] then
+		return obj, start
+	end
+	assert(tk == ">")
+	if name == "script" then
+		local s, e = str:find("</script>", start)
+		child[#child + 1] = str:sub(start, s-1);
+		return obj, e + 1
+	elseif name == "style" then
+		local s, e = str:find("</style>", start)
+		child[#child + 1] = str:sub(start, s-1);
+		return obj, e + 1
+	end
+	--child
+	while true do
+		local tk
+		back = start
+		tk, start = nexttoken(str, start)
+		if not tk then
+			break
+		elseif tk == "<" then --open
+			child[#child + 1], start = parsenode(str, back)
+		elseif tk == "</" then --close
+			name, start = nexttoken(str, start)
+			tk, start = nexttoken(str, start)
+			assert(tk, ">")
+			if name ~= obj.name then
+				start = back
+			end
+			break
+		else
+			tk, start = nexttext(str, back)
+			local raw = htmlunescape(tk)
+			child[#child + 1] = raw
+		end
+	end
+	return obj, start
+end
 
 M.parse = function(str)
-	str = trip(str)
-	str = htmlunescape(str)
-	return parse(str, 1)
+	local ok, node = pcall(parsenode, str, 1)
+	if ok then
+		return node
+	else
+		return nil, node
+	end
 end
 
 return M
